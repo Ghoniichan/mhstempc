e3c4598e75f685b1659718ee15e5f24c23f04064
John Carlos <johncarlosnny@gmail.com>
2025-03-17 11:36:36 +0800

    Initial MHSTEMPC.

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container mt-5">
+        <h1 className="text-primary">
+          Hello, I am using Bootstrap right now.
+        </h1>
+        <button className="btn btn-success">Press me cuh</button>
+      </div>
+    </>
+  )
+}
+
+export default App
