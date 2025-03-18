e3818fe7782818ca505c03acae666c4a82d753e4
John Carlos <johncarlosnny@gmail.com>
2025-03-18 18:15:33 +0800

    Added components folder with Sidebar contents.

---
+++ src/components/Dashboard/Sidebar.tsx
@@ -0,0 +1,13 @@
+import './Sidebar.css'
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <h1 className="text-primary">Sidebar</h1>
+      <button className="btn btn-success">Press me cuh</button>
+    </div>
+  )
+}
+
+export default Sidebar
