c2d583753ae32d70c32ad951d0763e4867660493
John Carlos <johncarlosnny@gmail.com>
2025-04-24 00:40:53 +0800

    Finished contents of the sidebar. Lacking styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Sidebar from './components/Dashboard/Sidebar'
+import Sidebar from './components/Sidebar'
 
 
 
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,35 @@
+import logo from '../assets/logo.png'
+import dashboard from '../assets/dashboard.png'
+import client from '../assets/client.png'
+import notif from '../assets/notification.png'
+import settings from '../assets/settings.png'
+
+const Sidebar = () => {
+  return (
+    <div className="col-md-5 col-lg-4 col-sm-4 bg-primary text-white vh-100 p-3">
+    <div className='mb-3 d-flex justify-content-center'>
+        <img src={logo} className="img-fluid"  alt="logo"/>
+    </div>
+    <ul className="nav flex-column ">
+      <li className="nav-item">
+        <img src={dashboard} className="img-fluid"  alt="logo"/>
+        <a href="#" className="nav-link text-white">Dashboard</a>
+      </li>
+      <li className="nav-item">
+        <img src={client} className="img-fluid"  alt="logo"/>
+        <a href="#" className="nav-link text-white">Client</a>
+      </li>
+      <li className="nav-item">
+        <img src={notif} className="img-fluid"  alt="logo"/>
+        <a href="#" className="nav-link text-white">Notifications</a>
+      </li>
+      <li className="nav-item">
+        <img src={settings} className="img-fluid"  alt="logo"/>
+        <a href="#" className="nav-link text-white">Settings</a>
+      </li>
+    </ul>
+  </div>
+  )
+}
+
+export default Sidebar
