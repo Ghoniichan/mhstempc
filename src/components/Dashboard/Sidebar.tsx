eb7db5c835b8763da88822ca8fc4c4d6543ce3b4
AelaBandasan <bandasanmichaela@gmail.com>
2025-04-26 21:46:02 +0800

    moved the sidebar.tsx inside the component folder

--- src/components/Dashboard/Sidebar.tsx
+++ src/components/Dashboard/Sidebar.tsx
@@ -1,18 +1,32 @@
-import './Sidebar.css'
+import logo from '../assets/logo.png'
+import dashboard from '../assets/dashboard.png'
+import client from '../assets/client.png'
+import notif from '../assets/notification.png'
+import settings from '../assets/settings.png'
+
 
 const Sidebar = () => {
   return (
-    <div className="col-md-3 col-lg-6 bg-primary text-white vh-100 p-3">
-    <h4>Sidebar</h4>
-    <ul className="nav flex-column">
+    <div className="col-md-5 col-lg-4 col-sm-4 bg-primary text-white vh-100 p-3">
+    <div className='mb-3 d-flex justify-content-center'>
+        <img src={logo} className="img-fluid"  alt="logo"/>
+    </div>
+    <ul className="nav flex-column ">
       <li className="nav-item">
+        <img src={dashboard} className="img-fluid"  alt="logo"/>
         <a href="#" className="nav-link text-white">Dashboard</a>
       </li>
       <li className="nav-item">
-        <a href="#" className="nav-link text-white">Settings</a>
+        <img src={client} className="img-fluid"  alt="logo"/>
+        <a href="#" className="nav-link text-white">Client</a>
       </li>
       <li className="nav-item">
-        <a href="#" className="nav-link text-white">Profile</a>
+        <img src={notif} className="img-fluid"  alt="logo"/>
+        <a href="#" className="nav-link text-white">Notifications</a>
+      </li>
+      <li className="nav-item">
+        <img src={settings} className="img-fluid"  alt="logo"/>
+        <a href="#" className="nav-link text-white">Settings</a>
       </li>
     </ul>
   </div>
