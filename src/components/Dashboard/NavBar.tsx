c9db70af5195a6492a5b75b8df329bd9f6dd58d3
AelaBandasan <bandasanmichaela@gmail.com>
2025-05-25 21:52:26 +0800

    fix navbar cutie

--- src/components/Dashboard/NavBar.tsx
+++ src/components/Dashboard/NavBar.tsx
@@ -1,14 +1,17 @@
 import { NavLink } from 'react-router-dom';
-// import logoNav from '../../../src/assets/images/mhstempc_logo.png';
-import '../../components/Dashboard/Navbar.css'; // If it's inside src/styles/
-
+import logo from '../../../src/assets/Images/mhstempc_logo.png';
+import '../../../src/components/Dashboard/Navbar.css'
 
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg fixed-top w-100">
     <div className="container-fluid">
-      {/* <img src={logoNav} alt="logo" className="img-fluid" style={{ maxHeight: '50px', paddingLeft: '15px' }} /> */}
-      <small className='Name alegreya-sans-extrabold'>MHSTEMPC</small>
+      <div className="navbar-brand-wrapper">
+                    <img src={logo} alt='navbar logo' className='img-fluid'
+                    style={{height: '50px', 
+                                paddingLeft: '15px'}}/>
+                <small className="Name alegreya-sans-extrabold">MHSTEMPC</small>
+                </div>
 
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
         <span className="navbar-toggler-icon"></span>
