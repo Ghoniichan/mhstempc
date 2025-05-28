fd7f3b8e13ea2fcc3216fe1f307e8d0b49901e3e
John Carlos <johncarlosnny@gmail.com>
2025-05-28 23:23:26 +0800

    Log In Authentication

---
+++ server/src/controllers/authController.ts
@@ -0,0 +1,36 @@
+// controllers/authController.ts
+import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import pool from '../config/db.config'; // Adjust path to your database connection
+import jwtGenerator from '../utils/jwtGenerator'; // Adjust path to your JWT utility
+
+export const login = async (req: Request, res: Response): Promise<void> => {
+  const { email, password } = req.body;
+  
+  try {
+    // Check if user exists
+    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [email]);
+    
+    if (user.rows.length === 0) {
+      res.status(401).json("Invalid Credential");
+      return;
+    }
+    
+    // Validate password
+    const validPassword = (password == user.rows[0].user_password)//await bcrypt.compare(password, user.rows[0].user_password);
+    
+    if (!validPassword) {
+      res.status(401).json("Invalid Credential");
+      return;
+    }
+    
+    // Generate JWT token
+    const jwtToken = jwtGenerator(user.rows[0].user_id);
+    
+    res.json({ jwtToken });
+    
+  } catch (err: any) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+};
