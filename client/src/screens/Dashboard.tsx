641d3a3d87bc1bf71f04a8143276a4ee6c92b639
AelaBandasan <bandasanmichaela@gmail.com>
2025-06-01 23:58:23 +0800

    screens addedd

--- client/src/screens/Dashboard.tsx
+++ client/src/screens/Dashboard.tsx
@@ -5,33 +5,36 @@ const Dashboard = () => {
     const navigate = useNavigate();
 
     return (
+      <div style={{ overflowX: "hidden" }}>
         <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        width: "100%",
-        marginLeft: "200px", 
-        padding: "45px 20px 450px 25px",
-        boxSizing: "border-box",
-      }}
-    >
-      {/* Page Title */}
-      <h3 style={{ marginBottom: "20px" }}>Dashboard</h3>
-
-      {/* Cards */}
-      <div
         style={{
           display: "flex",
-          gap: "10px",
-          flexWrap: "wrap",
+          flexDirection: "column",
+          width: "100%",
+          marginLeft: "200px", 
+          padding: "45px 20px 450px 25px",
+          boxSizing: "border-box",
         }}
       >
-        <SimpleCard title="Applications" onClick={() => navigate('/application')}/>
-        <SimpleCard title="Loans" onClick={() => navigate('/loans')}/>
-        <SimpleCard title="Payment" onClick={() => navigate('/payment')}/>
-        <SimpleCard title="Missed Payment" onClick={() => navigate('/missedPayment')}/>
+        {/* Page Title */}
+        <h3 style={{ marginBottom: "20px" }}>Dashboard</h3>
+
+        {/* Cards */}
+        <div
+          style={{
+            display: "flex",
+            gap: "10px",
+            flexWrap: "wrap",
+          }}
+        >
+          <SimpleCard title="Applications" onClick={() => navigate('/application')}/>
+          <SimpleCard title="Loans" onClick={() => navigate('/loans')}/>
+          <SimpleCard title="Payment" onClick={() => navigate('/payment')}/>
+          <SimpleCard title="Missed Payment" onClick={() => navigate('/missedPayment')}/>
+        </div>
       </div>
-    </div>
+        
+      </div>
     )
 }
 
