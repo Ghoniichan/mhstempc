7702c9a1b1960a6dc6c6f11be8160f6da639ab84
AelaBandasan <bandasanmichaela@gmail.com>
2025-06-02 22:42:34 +0800

    account screen added

--- client/src/screens/AccountScreen.tsx
+++ client/src/screens/AccountScreen.tsx
@@ -1,11 +1,13 @@
+import { Container } from 'react-bootstrap';
+import ProfileSection from '../components/Dashboard/ProfileSection';
 import '../screens/Account.css';
 
 const AccountScreen = () => {
-    return(
-        <div>
-            
-        </div>
-    )
-}
+    return (
+        <Container fluid className="account-screen px-4 py-4">
+            <ProfileSection />
+        </Container>
+    );
+};
 
-export default AccountScreen
+export default AccountScreen;
