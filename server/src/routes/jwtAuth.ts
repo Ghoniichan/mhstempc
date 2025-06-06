0eac3c1f4b4e4886e18b0534a92beeaa65beeeae
John Carlos <johncarlosnny@gmail.com>
2025-06-06 18:26:49 +0800

    Added jwt token to headers

--- server/src/routes/jwtAuth.ts
+++ server/src/routes/jwtAuth.ts
@@ -1,10 +1,12 @@
 // routes/authRoutes.ts
 import express from 'express';
-import { login, register } from '../controllers/authController';
+import { login, register, verify} from '../controllers/authController';
+import authorize from '../middleware/authorize';
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/register", register);
+router.post("/verify", authorize, verify);
 
 export default router;
