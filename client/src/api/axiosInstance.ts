a1abd6398e81173fb3b8ca39a325108f7aa717fa
John Carlos <johncarlosnny@gmail.com>
2025-06-06 16:44:40 +0800

    Added OR localhost at api

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 // src/api/axiosInstance.ts
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_APP_API_BASE_URL!;
+const baseURL = import.meta.env.VITE_APP_API_BASE_URL || 'http://localhost:9000';
 const instance = axios.create({ baseURL });
 export default instance;
