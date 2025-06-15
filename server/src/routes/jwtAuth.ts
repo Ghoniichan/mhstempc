91622fee20e3c02b39e3404ed4b323440c66ec7a
John Carlos <johncarlosnny@gmail.com>
2025-06-16 00:33:33 +0800

    Add password generation endpoint and update auth routes

--- server/src/routes/jwtAuth.ts
+++ server/src/routes/jwtAuth.ts
@@ -1,6 +1,6 @@
 // routes/authRoutes.ts
 import express from 'express';
-import { login, register, verify} from '../controllers/authController';
+import { login, register, verify, genPass} from '../controllers/authController';
 import authorize from '../middleware/authorize';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post("/login", login);
 router.post("/register", register);
 router.post("/verify", authorize, verify);
+router.post("/gen-pass/:pass", genPass);
 
 export default router;
