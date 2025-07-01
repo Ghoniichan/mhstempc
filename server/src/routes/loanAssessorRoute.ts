f5182bc5ba1242149d31e7802112ec62c1fba1e9
John Carlos <johncarlosnny@gmail.com>
2025-07-01 23:11:25 +0800

    refactor: Simplify totals fetching logic and remove unnecessary database transactions

--- server/src/routes/loanAssessorRoute.ts
+++ server/src/routes/loanAssessorRoute.ts
@@ -16,10 +16,7 @@ router.get('/totals/:policy_num', async (req, res) => {
     return;
   }
 
-  const client = await pool.connect();
   try {
-    await client.query('BEGIN');
-
     const query = `
       SELECT
         COALESCE(SUM(cs.amount), 0) AS total_capital_share,
@@ -32,52 +29,26 @@ router.get('/totals/:policy_num', async (req, res) => {
 
     const result = await pool.query(query, [policy_num]);
     const totals = result.rows[0];
-    const total_capital_share = parseFloat(totals.total_capital_share);
-    const total_savings = parseFloat(totals.total_savings);
 
     res.status(200).json({
       policy_number: policy_num,
-      totalCapitalShare: total_capital_share,
-      totalSavings: total_savings
+      totalCapitalShare: parseFloat(totals.total_capital_share),
+      totalSavings: parseFloat(totals.total_savings)
     });
-
-    await client.query('COMMIT');
   } catch (error) {
     console.error("Error fetching totals:", error);
     res.status(500).json({ error: "Internal Server Error" });
-  } finally {
-    client.release();
   }
 });
 
 router.post('/assess', async (req, res) => {
-
-  const client = await pool.connect();
   try {
-    await client.query('BEGIN');
-    const { loan_amount, policy_num } = req.body;
-
-    const query = `
-    SELECT
-      COALESCE(SUM(cs.amount), 0) AS total_capital_share,
-      COALESCE(SUM(s.received_amount - s.withdrawal), 0) AS total_savings
-    FROM membership_applications m
-    LEFT JOIN capital_share cs ON m.id = cs.membership_id
-    LEFT JOIN savings s ON m.id = s.membership_id
-    WHERE m.policy_number = $1
-  `;
-
-    const result = await pool.query(query, [policy_num]);
-    const totals = result.rows[0];
-    const total_capital_share = parseFloat(totals.total_capital_share);
-    const total_savings = parseFloat(totals.total_savings);
-    console.log(`Total Capital Share: ${total_capital_share}, Total Savings: ${total_savings}`);
-
+    const { loan_amount, capital_share, savings } = req.body;
     // Forward data to Python FastAPI server
     const response = await axios.post(`${loanAssessorUrl}`, {
       loan_amount,
-      total_capital_share,
-      total_savings,
+      capital_share,
+      savings,
     });
 
     // Send back the result from Python to the client
@@ -85,9 +56,6 @@ router.post('/assess', async (req, res) => {
   } catch (error) {
     console.error('Error connecting to loan assessor:', error);
     res.status(500).json({ error: 'Loan assessor service unavailable.' });
-  } finally {
-    await client.query('COMMIT');
-    client.release();
   }
 });
 
